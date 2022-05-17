32b2a36a37967c6d0eee4500e41b890c7b7f810d
Koenig <lkoenigsnyder@usgs.gov>
2022-05-17 15:59:11 -0400

    group siteids for downloading data in chunks

--- 1_fetch.R
+++ 1_fetch.R
@@ -95,11 +95,17 @@ p1_targets_list <- list(
       pull(MonitoringLocationIdentifier) %>%
       unique()
   ),
+  
+  # Group the sites into reasonably sized chunks for downloading data 
+  tar_target(
+    p1_site_ids_grouped,
+    add_download_groups(p1_site_ids, max_sites_allowed = 500)
+  ),
 
   # Download data using output of WQP inventory  
   tar_target(
     p1_wqp_data_aoi,
-    fetch_wqp_data(p1_site_ids, p1_char_names, 
+    fetch_wqp_data(p1_site_ids_grouped, p1_char_names, 
                    query_site_limit = 500, wqp_args = wqp_args)
   )
 
--- 1_fetch/src/fetch_wqp_data.R
+++ 1_fetch/src/fetch_wqp_data.R
@@ -1,3 +1,29 @@
+#' Group sites for downloading data without hitting the WQP cap
+#' 
+#' @description This function returns a data frame with a single column used
+#' to group the sites into reasonably sized chunks for downloading data.
+#' 
+#' @param siteids vector of character strings containing site identifiers
+#' @param max_sites_allowed integer indicating the maximum number
+#' of sites allowed in each download group. Defaults to 500.
+#' 
+#' @return returns a data frame with columns site id, site number, and an 
+#' additional column called `download_grp` which is made up of unique groups 
+#' to `group_by()` and then `tar_group()` for downloading.
+#' 
+add_download_groups <- function(siteids, max_sites_allowed = 500) {
+  
+  siteids_df <- tibble(site_id = siteids) %>%
+    mutate(site_n = row_number()) %>%
+    # Add a column indicating which chunk a site belongs to; the number of rows
+    # in each chunk should not exceed `max_sites_allowed`
+    mutate(download_grp = ((site_n -1) %/% max_sites_allowed) + 1)
+  
+  return(siteids_df)
+
+}
+
+
 #' Download data from the Water Quality Portal
 #' 
 #' @description Function to pull WQP data given a vector of site ids
@@ -14,7 +40,7 @@
 #' defaults to NULL. See https://www.waterqualitydata.us/webservices_documentation 
 #' for more information.  
 #' 
-#' @value returns a data frame containing data downloaded from the Water Quality Portal, 
+#' @return returns a data frame containing data downloaded from the Water Quality Portal, 
 #' where each row represents a unique data record. 
 #' 
 fetch_wqp_data <- function(siteids, characteristics, query_site_limit = 500, 
