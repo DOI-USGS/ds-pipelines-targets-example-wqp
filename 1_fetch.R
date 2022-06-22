d07922c80c3ee8f886f7c72d7c96a19d78642b93
Koenig <lkoenigsnyder@usgs.gov>
2022-06-22 09:21:51 -0400

    expand conus grid to all of USA

--- 1_fetch.R
+++ 1_fetch.R
@@ -54,22 +54,22 @@ p1_targets_list <- list(
   
   # Create a big grid of boxes to set up chunked data queries
   tar_target(
-    p1_conus_grid,
-    create_conus_grid(cellsize = c(1,1))
+    p1_nationwide_grid,
+    create_nationwide_grid(cellsize = c(1,1))
   ),
   
   # Use spatial subsetting to find boxes that overlap the area of interest
   # (i.e., are within dist_m of p1_AOI_sf). These boxes will be used to
   # query the WQP.
   tar_target(
-    p1_conus_grid_aoi,
-    subset_grids_to_aoi(p1_conus_grid, p1_AOI_sf, dist_m = 5000)
+    p1_nationwide_grid_aoi,
+    subset_grids_to_aoi(p1_nationwide_grid, p1_AOI_sf, dist_m = 5000)
   ),
   
   # Inventory data available from the WQP within each of the boxes that overlap
   # the area of interest. To prevent timeout issues that result from large data 
   # requests, use {targets} dynamic branching capabilities to map the function 
-  # inventory_wqp() over each grid within p1_conus_grid_aoi. {targets} will 
+  # inventory_wqp() over each grid within p1_nationwide_grid_aoi. {targets} will 
   # then combine all of the grid-scale inventories into one table.
   tar_target(
     p1_wqp_inventory,
@@ -77,10 +77,10 @@ p1_targets_list <- list(
     # also pass additional arguments to WQP, e.g. sampleMedia or siteType, using 
     # wqp_args. Below, wqp_args is defined in _targets.R. See documentation
     # in 1_fetch/src/get_wqp_inventory.R for further details.
-    inventory_wqp(grid = p1_conus_grid_aoi,
+    inventory_wqp(grid = p1_nationwide_grid_aoi,
                   char_names = p1_char_names,
                   wqp_args = wqp_args),
-    pattern = map(p1_conus_grid_aoi)
+    pattern = map(p1_nationwide_grid_aoi)
   ),
   
   # Subset the WQP inventory to only retain sites within the area of interest
--- 1_fetch/src/create_grids.R
+++ 1_fetch/src/create_grids.R
@@ -1,35 +1,50 @@
-#' Create CONUS grid
-#' 
+#' Create a nationwide grid including AK/HI and territories
+#'  
 #' @description Function to create a big grid of boxes to use for chunked
 #' data queries
 #' 
 #' @param cellsize the target cell size of each box in map units (here, degrees);
 #' takes two values indicating the size in the x direction and the size in the 
-#' y direction, see ??sf::st_make_grid for further details.
+#' y direction. Defaults to 2 degree cell size. See ??sf::st_make_grid for 
+#' further details.
 #' @param year integer; year of the state polygon data download (defaults to 2020); 
 #' see ??tigris::states for further details.
 #' @param progress_bar logical; indicates whether a progress bar showing the status
 #' of the state polygon build should be returned (defaults to FALSE)
 #' 
 #' @value returns an sf polygon object containing the geometries and an attribute 
-#' id for each box within the CONUS grid.
+#' id for each box within the nationwide grid.
 #' 
-#' @example create_conus_grid(cellsize = c(1,1))
+#' @example create_nationwide_grid(cellsize = c(2,2))
 #' 
 
-create_conus_grid <- function(cellsize, year = 2020, progress_bar = FALSE){
+create_nationwide_grid <- function(cellsize = c(2,2), year = 2020, progress_bar = FALSE){
  
-  conus_grid <- tigris::states(class = "sf", year = year, progress_bar = progress_bar) %>%
-    # filter state polygons to CONUS extent
-    filter(REGION != "9", 
-           !STUSPS %in% c("HI","AK")) %>%
-    # create square grid with cell sizes equal to 1 deg. x 1 deg.
-    sf::st_make_grid(cellsize = c(cellsize[1],cellsize[2]), square = TRUE) %>%
+  usa_sf <- tigris::states(class = "sf", year = year, progress_bar = progress_bar)
+  
+  # create square grid with cell sizes equal to `cellsize` passed in degrees.
+  usa_grid <- usa_sf %>%
+    sf::st_make_grid(cellsize = c(cellsize[1],cellsize[2]), square = TRUE, 
+                     # Start lower left of grid right at 180th meridian to avoid
+                     # issues with WQP query bboxes crossing from E longitudes to W
+                     # See https://github.com/USGS-R/dataRetrieval/issues/616
+                     offset = c(-180, sf::st_bbox(usa_sf)$ymin)) %>%
     # convert to sf object and add an "id" attribute
     sf::st_as_sf() %>%
     mutate(id = row.names(.))
   
-  return(conus_grid)
+  # Check that usa_grid produces a valid bounding box to pass to WQP. Check that
+  # the west-east coordinates don't cross 180. Note that this behavior could 
+  # result from picking a cellsize[1] that 180 is not evenly divisible by (like 7)
+  # because it results in a grid cell crossing the 180th meridian.
+  grid_xmaxes <- purrr::map(usa_grid$x, function(grid) st_bbox(grid)$xmax)
+  if(any(unlist(grid_xmaxes) > 180)) {
+    stop("Grid cells cross over 180 deg meridian and will cause WQP 
+    calls to fail. Change `cellsize[1]` so that 180 is divisible 
+    by it or make adjustments to `create_national_grid()`")
+  }
+  
+  return(usa_grid)
   
 }
 
@@ -50,7 +65,7 @@ create_conus_grid <- function(cellsize, year = 2020, progress_bar = FALSE){
 #' @value returns an sf polygon object containing the geometries for each box 
 #' within the holistic grid that overlaps the buffered area of interest.
 #' 
-#' @example subset_grids_to_aoi(grid = p1_conus_grid, dist_m = 5000)
+#' @example subset_grids_to_aoi(grid = p1_nationwide_grid, dist_m = 5000)
 #' 
 
 subset_grids_to_aoi <- function(grid, aoi_poly, dist_m){
