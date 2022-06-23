cc427b059c562b9799b795669d1fc08eb8aad478
Koenig <lkoenigsnyder@usgs.gov>
2022-06-23 13:50:47 -0400

    coerce all columns to character when returning downloaded data (#47)

--- 1_fetch/src/fetch_wqp_data.R
+++ 1_fetch/src/fetch_wqp_data.R
@@ -29,9 +29,11 @@ add_download_groups <- function(sitecounts_df, max_sites_allowed = 500) {
 
 #' Download data from the Water Quality Portal
 #' 
-#' @description Function to pull WQP data given a vector of site ids
+#' @description Function to pull WQP data given a dataset of site ids
 #'  
-#' @param siteids_grouped vector of character strings containing site identifiers
+#' @param site_counts_grouped data frame containing site identifiers, the 
+#' total number of records, site numbers, and a download group assigned
+#' for each site. Must contain columns `site_id` and `site_n`.
 #' @param characteristics vector of character strings indicating which WQP
 #' CharacteristicName to query
 #' @param wqp_args list containing additional arguments to pass to whatWQPdata(),
@@ -41,18 +43,26 @@ add_download_groups <- function(sitecounts_df, max_sites_allowed = 500) {
 #' @return returns a data frame containing data downloaded from the Water Quality Portal, 
 #' where each row represents a unique data record. 
 #' 
-fetch_wqp_data <- function(siteids_grouped, characteristics, wqp_args = NULL){
+fetch_wqp_data <- function(site_counts_grouped, characteristics, wqp_args = NULL){
   
   message(sprintf("Retrieving WQP data for sites %s:%s",
-                  min(siteids_grouped$site_n), max(siteids_grouped$site_n)))
+                  min(site_counts_grouped$site_n), 
+                  max(site_counts_grouped$site_n)))
   
   # Define arguments for readWQPdata
-  wqp_args_all <- c(wqp_args, list(siteid = siteids_grouped$site_id,
+  wqp_args_all <- c(wqp_args, list(siteid = site_counts_grouped$site_id,
                                    characteristicName = c(characteristics)))
   
   # Pull data
   wqp_data <- dataRetrieval::readWQPdata(wqp_args_all)
   
-  return(wqp_data)
+  # Some records return character strings when we expect numeric values, 
+  # e.g. when "*Non-detect" appears in the "ResultMeasureValue" field. 
+  # For now, consider all columns to be character so that individual data
+  # frames returned from fetch_wqp_data can be joined together. 
+  wqp_data_out <- wqp_data %>%
+    mutate(across(everything(), as.character))
+  
+  return(wqp_data_out)
 }
 
