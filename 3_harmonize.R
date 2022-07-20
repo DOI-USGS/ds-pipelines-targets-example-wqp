d1084d9057e9405943f25802ecf96e6bc2d2a357
Koenig <lkoenigsnyder@usgs.gov>
2022-07-20 17:03:14 -0400

    omit save_NA_vals() from format_columns.R

--- 3_harmonize.R
+++ 3_harmonize.R
@@ -4,17 +4,15 @@ source("3_harmonize/src/format_columns.R")
 p3_targets_list <- list(
   
   # All columns in p2_wqp_data_aoi are of class character. Coerce select columns
-  # back to numeric, and save an output file that contains any entries that could 
-  # not be parsed as numeric. The default option is to format "ResultMeasureValue" 
-  # and "DetectionQuantitationLimitMeasure.MeasureValue" to numeric, but additional
+  # back to numeric, but first retain original entries in new columns ending in 
+  # "_original". The default option is to format "ResultMeasureValue" and 
+  # "DetectionQuantitationLimitMeasure.MeasureValue" to numeric, but additional
   # variables can be added using the `vars_to_numeric` argument in format_columns().
-  # For any columns parsed to numeric, the original entries are retained in columns
-  # ending in "_original." By default, format_columns() will retain all columns, 
-  # but undesired variables can also be dropped from the WQP dataset using the 
-  # optional `drop_vars` argument. 
+  # By default, format_columns() will retain all columns, but undesired variables 
+  # can also be dropped from the WQP dataset using the optional `drop_vars` argument. 
   tar_target(
     p3_wqp_data_aoi_formatted,
-    format_columns(p2_wqp_data_aoi, save_dir = "3_harmonize/out")
+    format_columns(p2_wqp_data_aoi)
   )
 
 )
--- 3_harmonize/src/format_columns.R
+++ 3_harmonize/src/format_columns.R
@@ -8,13 +8,11 @@
 #' 
 #' @param wqp_data data frame containing the data downloaded from the WQP, 
 #' where each row represents a unique data record.
-#' @param save_dir character string indicating the directory where an output
-#' file should be saved. The output file is intended to inform the user of any
-#' WQP entries in columns `vars_to_numeric` that were not able to be parsed to
-#' numeric values and so were replaced with NA.
 #' @param vars_to_numeric character string indicating which column(s) should be 
 #' coerced to class numeric. Default includes two columns: `ResultMeasureValue` 
-#' and `DetectionQuantitationLimitMeasure.MeasureValue`.
+#' and `DetectionQuantitationLimitMeasure.MeasureValue`. Note that for all
+#' columns coerced to numeric, the original entries will be retained in new
+#' columns ending in "_original."
 #' @param drop_vars optional; character string indicating any undesired
 #' variables that should be dropped from the WQP dataset. Default is to retain 
 #' all variables. Note that certain variables are used in downstream 
@@ -27,7 +25,6 @@
 #' Quality Portal, where each row represents a unique data record.
 #' 
 format_columns <- function(wqp_data, 
-                           save_dir,
                            vars_to_numeric = c('ResultMeasureValue',
                                                'DetectionQuantitationLimitMeasure.MeasureValue'),
                            drop_vars = NULL){
@@ -63,68 +60,8 @@ format_columns <- function(wqp_data,
     suppressWarnings() %>%
     # drop any undesired columns 
     select(-c(any_of(drop_vars)))
-    
-  # Save a file containing which values were replaced with NA when column
-  # was converted from character to numeric  
-  save_NA_vals(wqp_data, save_dir, vars_to_numeric)
   
   return(wqp_data_out)
 }
 
-
-#' @title Record values that have been replaced with NA
-#' 
-#' @description
-#' Function to save an output file that contains any values that were replaced
-#' with NA when a column was converted from class character to class numeric. 
-#' Values were replaced with NA if the original entry could not be parsed as 
-#' a numeric value.
-#' 
-#' @param wqp_data data frame containing the data downloaded from the WQP, 
-#' where each row represents a unique data record.
-#' @param save_dir character string indicating the directory where an output
-#' file should be saved. The output file is intended to inform the user of any
-#' WQP entries in columns `vars_to_numeric` that were not able to be parsed to
-#' numeric values and so were replaced with NA.
-#' @param vars_to_numeric character string indicating which column(s) should be 
-#' coerced to class numeric. 
-#' 
-#' @returns 
-#' Returns a character string indicating the file path of the saved output file.
-#' The saved .txt file includes a section for each column in `vars_to_numeric` 
-#' that contains the original WQP entries for that column that were replaced with 
-#' NA when the column was converted from class character to class numeric.
-#' 
-save_NA_vals <- function(wqp_data, save_dir, vars_to_numeric){
-
-  # Find any values from the original columns that were replaced with NA
-  values_replaced_with_na <- lapply(vars_to_numeric, function(x){
-    
-    vars_vals_na <- wqp_data %>%
-      filter(!is.na(.data[[x]]),
-             is.na(as.numeric(.data[[x]]))) %>%
-      suppressWarnings() %>%
-      pull(.data[[x]]) %>% 
-      unique()
-    
-    if(length(vars_vals_na) > 0){
-      out <- sprintf(paste0("The following entries for ", x, " cannot be ",
-                            "parsed as numeric and will be converted to ",
-                            "NA:\n\n%s\n"),
-                     paste(unique(vars_vals_na), collapse = "\n"))
-    } else {
-      out <- paste0("Good news! All entries for ", x, " could be parsed as ",
-                    "numeric. \n")
-    }
-    return(out)
-  })
-
-  # Save a file containing which values were replaced with NA when column
-  # was converted from character to numeric
-  out_file <- paste0(save_dir,"/WQP_entries_replaced_with_NA.txt")
-  writeLines(paste(unlist(lapply(values_replaced_with_na, paste, collapse = " "))), 
-             out_file)
-  
-  return(out_file)
-}
 
